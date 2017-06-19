8cfef27b35218548f901e1baa403a6eaae2c153d
William Lotosky <wlotosky@gmail.com>
2017-06-19 12:58:17 -0400

    responsive, partials, jquery

--- public/scritps/main.js
+++ public/scritps/main.js
@@ -1,3 +1,19 @@
-"use strict";
+'use strict';
 
-$(document).ready(function () {});
+$(document).ready(function () {
+
+	$('.hamburger').on('click', function (event) {
+		$('.hamburger').parent().toggleClass('modal slideInUp');
+	});
+
+	$(document).on('click touchstart', 'a[href^="#"]', function (event) {
+
+		event.preventDefault();
+
+		var targetId = $(this).attr('href');
+
+		var position = $(targetId).offset().top;
+
+		$('body, html').animate({ scrollTop: position }, 1500);
+	});
+});
